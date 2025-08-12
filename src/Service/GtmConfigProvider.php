4e10b6dfc2090d75fbfe88487f6c5b87d71abb37
Patryk Tchórzewski <patryk@dark-side.pro>
2025-08-12 20:19:35 +0200

    Refactor: Use GtmConfigProvider for GTM container ID in GtmHeadSubscriber. Improve DI and configuration flexibility.

--- src/Service/GtmConfigProvider.php
+++ src/Service/GtmConfigProvider.php
@@ -2,19 +2,10 @@
 
 namespace DarkSidePro\SyliusGtmPlugin\Service;
 
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-
 final class GtmConfigProvider
 {
-    private ParameterBagInterface $params;
-
-    public function __construct(ParameterBagInterface $params)
-    {
-        $this->params = $params;
-    }
-
     public function getContainerId(): ?string
     {
-        return $this->params->get('env(GTM_CONTAINER_ID)');
+        return $_ENV['GTM_CONTAINER_ID'] ?? null;
     }
 }
